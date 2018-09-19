4bfa1767755b21d93a18b7e4736e69b79eb1cf10
Fernando Souza <morphors@gmail.com>
2018-09-19 20:32:03 -0300

    resolucao da professora exercicio tabuleiro aula8

--- aula8-exercicio2.c
+++ aula8-exercicio2.c
@@ -9,15 +9,15 @@ numero primo
 int main(){
     
     int tabuleiro = 31;
-    int dolar = 4;
-    long long int pagamento;
+    float pagamento, dolar;
     
-    pagamento = dolar;
+    // pagamento = 0;
+    dolar = 1;
     
-    for (int i = 2; i <= tabuleiro; i++){
-        // 
-        pagamento = pagamento * 2;
-        printf("\nna casa %d o valor eh: %lld!\n", i, pagamento);
+    for (int i = 1; i <= tabuleiro; i++){
+        //pagamento = pagamento + dolar;
+        dolar = dolar * 2;
+        printf("\nna casa %d o valor eh: %.0f \n", i, dolar);
     }
   // mais comentários
 }
