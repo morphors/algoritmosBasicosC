9d4d8a1ea72db2cc961c4c3e7df836110c0e0a6a
Fernando Souza <morphors@gmail.com>
2018-10-11 19:33:39 -0300

    alteracoes AV1

--- aula12-atividade-AV1.c
+++ aula12-atividade-AV1.c
@@ -1,36 +1,40 @@
+#include<stdio.h>
+
 /*
 
-Em um cercado há vários patos e coelhos. Escreva um programa que solicite ao usuário o total de
-cabeças e o total de pés, e determine quantos patos e coelhos se encontram nesse cercado
+Em um cercado há vários Flamingos e Cavalos. Escreva um programa que solicite ao usuário o total de
+cabeças e o total de pés, e determine quantos Flamingos e Cavalos se encontram nesse cercado
 
 */
 
-#include<stdio.h>
 
 int main(){
-  
-  int flamingos, cavalos, cabecas, patas;
-  
-  printf("\n\Diga o numero de cabecas: ");
-  scanf("%d", &cabecas);
-  
-  printf("\n\Diga o numero de patas: ");;
-  scanf("%d", &patas);
-  
-  cavalos = (patas-(2*cabecas))/2;
-  flamingos = cabecas - cavalos;
-  
-  if(cabecas > patas || patas < cabecas || cavalos <= 0){
+    int flamingos, cavalos, cabecas, patas;
     
-    printf("\nNao eh possivel!");
+    printf("\n \t\t Contando quantos animais de cada especie existe em um cercado. \n\n");
+    printf(" Considere - Flamingos (2 patas) \t Cavalos (4 patas) \n\n ");
+    printf("\t Considerando que não há somente uma espécie no cercado. Pelo menos 1 flamingo e 1 cavalo \n");
     
-  }
-  else{
+    printf("Digite abaixo a quantidade de cabecas e patas:");
+    printf("\n Qual o numero total de cabecas: ");
+    scanf("%d", &cabecas);
+
+    printf("\n Qual o numero total de patas: ");
+    scanf("%d", &patas);
     
-  printf("\n\nO numero de cavalos eh %d e o de flamingos eh %d", cavalos, flamingos);
+    cavalos = (patas-(2*cabecas))/2;
+    flamingos = cabecas - cavalos;
+	
+    if(cabecas > patas || patas < cabecas || cavalos <= 0 || flamingos <= 0){ // restricoes
+	    printf("\nNao eh possivel!\n ");
+	    printf("\n- O numero de cabecas deve ser menor que o de patas!\n ");
+	    printf("\n- O numero de patas deve ser maior do que o de cabecas!\n ");
+    }
+	
+    else{
+	    printf("\nResultado: \n ");
+	    printf("O numero de cavalos: %d \nO numero de flamingos: %d\n", cavalos, flamingos);
+    }
   
-  }
-  
-  return 0;
-  
+    return 0;
 }
