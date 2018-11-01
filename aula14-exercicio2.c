4b334074717376bcc4ec3b6c4f5c065fc3eec9fc
Fernando Souza <morphors@gmail.com>
2018-11-01 20:23:19 -0300

    exercicio 2 funcionando - sem resolucao

--- aula14-exercicio2.c
+++ aula14-exercicio2.c
@@ -6,29 +6,41 @@
  *
 */
 
-switch(Origem){
-	case 1:
-		printf("\n %f Produto do Sul", Preco);
-		break;
-	case 2:
-		printf("\n %f Produto do Norte", Preco);
-		break;
-	case 3:
-		printf("\n %f Produto do Leste", Preco);
-		break;
-	case 4:
-		printf("\n %f Produto do Oeste", Preco);
-		break;
-	case 5:
-		printf("\n %f Produto do Sudeste", Preco);
-		break;
-	case 6:
-		printf("\n %f Produto do Centro-Oeste", Preco);
-		break;
-	case 7:
-		printf("\n %f Produto do Nordeste", Preco);
-		break;
-	default:
-		printf("\n %f Produto importado", Preco);
-		break;
+int main(){
+    
+    int Origem;
+    float Preco;
+    
+    printf("\n Qual o código de origem: ");
+    scanf("%d", &Origem);
+    printf("\n Qual o preço do produto: ");
+    scanf("%f", &Preco);
+    
+    switch(Origem){
+        case 1:
+            printf("\n %f Produto do Sul", Preco);
+            break;
+        case 2:
+            printf("\n %f Produto do Norte", Preco);
+            break;
+        case 3:
+            printf("\n %f Produto do Leste", Preco);
+            break;
+        case 4:
+            printf("\n %f Produto do Oeste", Preco);
+            break;
+        case 5:
+            printf("\n %f Produto do Sudeste", Preco);
+            break;
+        case 6:
+            printf("\n %f Produto do Centro-Oeste", Preco);
+            break;
+        case 7:
+            printf("\n %f Produto do Nordeste", Preco);
+            break;
+        default:
+            printf("\n %f Produto importado", Preco);
+            break;
+    }
+    return 0;
 }
