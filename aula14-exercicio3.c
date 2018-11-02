05df7f4d4a0e972f0567164005b96d5b965e5cfc
Fernando Souza <morphors@gmail.com>
2018-11-01 21:50:48 -0300

    exercicio 3 - quadrado mais soma  resolução

---
+++ aula14-exercicio3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Crie uma função que retorna o seguinte: A função recebe três valores reais por parâmetro e retorna
+ * o quadrado do 1º + a soma dos outros dois números. 
+ *
+ * solução
+*/
+
+float calcula(int x, int y, int z){
+    return (x*x)+(y+z);
+}
+
+int main(){
+	// inicio 
+	int num1,num2,num3;
+    float resultado;
+
+	// entrada de dados
+	printf("Digite um numero: ");
+	scanf("%d", &num1);
+	printf("Digite um numero: ");
+	scanf("%d", &num2);
+	printf("Digite um numero: ");
+	scanf("%d", &num3);
+
+	// Operacoes (processamento)
+    resultado = calcula(num1,num2,num3);
+    
+    printf("\n O resultado: %.2f \n", resultado);
+
+	return(0);
+}
+
